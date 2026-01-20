77216a3afd56c7e41e90d95cb8a37fef06fb38a0
galina <galina.krupitsa@gmail.com>
2026-01-20 22:25:47 +0500

    db

---
+++ src/main/java/org/example/userservice/Controller/UserController.java
@@ -0,0 +1,18 @@
+package org.example.userservice.Controller;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @GetMapping("/{userId}")
+    public Long getUser(@PathVariable Long userId) {
+        return userId;
+    }
+}
