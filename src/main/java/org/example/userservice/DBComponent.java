3ea34f59b947f1e834581546e353086c439eca24
galina <galina.krupitsa@gmail.com>
2026-01-21 21:53:49 +0500

    get

---
+++ src/main/java/org/example/userservice/DBComponent.java
@@ -0,0 +1,26 @@
+package org.example.userservice;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBComponent {
+
+        private final JdbcTemplate jdbcTemplate;
+
+        public DBComponent(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+    @PostConstruct
+        public void checkConnection() {
+            Integer result = jdbcTemplate.queryForObject(
+                    "SELECT 1",
+                    Integer.class
+            );
+            System.out.println("JDBC CONNECTION OK, RESULT = " + result);
+        }
+    }
+
+
